d0e20b6dab33a2c6596393142ce3e7ca9fbb1a86
Swastik <swastikpanda32@gmail.com>
2023-12-05 02:28:00 +0530

    Added logic for balance tracker

--- fetch-challenges/src/App.jsx
+++ fetch-challenges/src/App.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const App = () => {
+const BalanceTracker = () => {
+  
+  const [balance, setBalance] = useState({
+    previousBalance: 1000
+  })
+  const [currentBalance, setCurrentBalance] = useState("");
+  const [transactionDetails, setTransactionDetails] = useState(null)
+  
+  const handleChange = (e) => {
+    setCurrentBalance(e.target.value)
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const transactionAmount = parseFloat(currentBalance);
+
+    if (!isNaN) {
+      const newTransaction = balance.previousBalance + transactionAmount;
+      setBalance({ ...balance, previousBalance: newTransaction })
+      setCurrentBalance("");
+
+      setTransactionDetails({
+      amount: transactionAmount,
+      date: new Date().toLocaleString()
+      })
+      
+    } else {
+      alert("Please provide a valid number");
+    }
+    
+  }
+  
+  
   return (
     <div>
-      <h1>React Challenges</h1>
+      <h1>Transactions</h1>
+      <label>Previous Day Transaction: 
+        <span>{balance.previousBalance}</span>
+      </label>
+      <br />
+      <label>Current Day Transaction:
+        <input type="number"
+          value={currentBalance}
+          onChange={handleChange}
+        />
+        
+      </label>
+      <br />
+      <button onClick={handleClick}>Add Transaction</button>
+      <br />
+      <label>Current Transaction:
+        <b>{transactionDetails && transactionDetails.amount}</b>
+        <br />
+        Date & Time: ({transactionDetails && transactionDetails.date})
+      </label>
+      <br />
+
+      <label>Total Transaction:
+        <strong>{ balance.previousBalance}</strong>
+      </label>
     </div>
   )
 }
 
-export default App
+export default BalanceTracker;
